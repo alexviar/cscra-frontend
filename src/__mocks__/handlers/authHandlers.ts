6a78be5e093b3c93c3c6d6976f8cac4f0da27ee3
Alexander <alexanderviar@gmail.com>
2021-07-08 15:02:40 -0400

    0807

--- src/__mocks__/handlers/authHandlers.ts
+++ src/__mocks__/handlers/authHandlers.ts
@@ -13,13 +13,23 @@ export const authHandlers = [
       // Respond with a 200 status code
       ctx.status(200),
       ctx.json({
-        id: 1,
-        username: 'admin',
-        roles: [{
-          id: 1,
-          name: 'super user'
-        }]
-      })
+        id:1,
+        ciRaiz:0,
+        ciComplemento:null,
+        apellidoPaterno:null,
+        apellidoMaterno:null,
+        nombres:"",
+        username:"admin",
+        estado:1,
+        regionalId:1,
+        createdAt:"2021-05-25",
+        updatedAt:"2021-05-25",
+        nombreCompleto:"",
+        ci:"0",
+        estadoText:"Activo",
+        allPermissions:[],
+        roles:[{
+          id:1,name:"super user",description:null,guardName:"sanctum",createdAt:"2021-05-25T17:23:24.000000Z",updatedAt:"2021-05-25T17:23:24.000000Z","pivot":{"modelId":1,"roleId":1,"modelType":"App\\Models\\User"},"permissions":[]}],"permissions":[]})
     )
   })
 ]
